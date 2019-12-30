eee89fe66d515950b90640397507e8a7446e7919
Robert Sundström <robert.sundstrom@outlook.com>
2019-12-30 15:20:32 +0100

    Disable nullable in custom behaviors

--- App1/App1/Converters/SelectedItemEventArgsToSelectedItemConverter.cs
+++ App1/App1/Converters/SelectedItemEventArgsToSelectedItemConverter.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#nullable disable
+
+using System;
 using System.Globalization;
 
 using Xamarin.Forms;
@@ -10,7 +12,7 @@ namespace App1.Converters
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var eventArgs = value as SelectedItemChangedEventArgs;
-            return eventArgs.SelectedItem;
+            return eventArgs?.SelectedItem;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
